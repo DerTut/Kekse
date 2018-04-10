011ddc02522d63666b5ce6812cc04bf980b41544
Friedtjof Wilms <WilmsFri@BAIT.BBW.DD>
2018-04-10 16:16:58 +0200

    Neues Projekt FizzBuzz
    • Program.cs erstellt - keine Änderungen
    • Problemloesung.cs erstellt - 2 neue Dictionary,  1 Funktionsaufruf
    • Test_Problemloesung erstellt - ersten Gerüsttest erstellt (noch nicht funktionstüchtig

---
+++ fizzbuzz/problemloesung/Problemloesung.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemloesung
+{
+    public class Problemloesung
+    {
+        public static Dictionary<int, string> Woerterbuch_start(int untergrenze, int obergrenze)
+        {
+            var woerterbuch = Erstelle_Woerterbuch(untergrenze, obergrenze);
+            return woerterbuch;
+        }
+
+        internal void Erstelle_Woerterbuch(int untergrenze, int obergrenze)
+        {
+
+            for (int i = untergrenze; i < obergrenze; i++)
+            {
+
+            }
+
+            Dictionary<int, string> woerterbuch_liste = new Dictionary<int, string>();
+            woerterbuch_liste.Add(3);
+            woerterbuch_liste.Add(5);
+
+            //woerterbuch_liste.Add(3, "Fuss");
+            //woerterbuch_liste.Add(5, "Buss");
+            //woerterbuch_liste.Add(3, "Fuss");
+        }
+    }
+}
---
+++ fizzbuzz/test_problemloesung/Test_Problemloesung.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using problemloesung;
+using NUnit.Framework;
+
+namespace test_problemloesung
+{
+    [TestFixture]
+    public class Test_Problemloesung
+    {
+        [Test, Category("Gerüsttest")]
+        public void anzahl_zahlen()
+        {
+            var ergebnis = Problemloesung.zahltest(1, 45);
+            Assert.AreEqual(45, ergebnis.Count());
+        }
+    }
+}
