056136e88ae3cf01f3b268ece8b048443bf1ff66
Friedtjof Wilms <WilmsFri@BAIT.BBW.DD>
2018-04-10 08:25:23 +0200

    Testimplementation
    Tests mit NUnit

--- kekse/test_kekse/Test_Kekse.cs
+++ kekse/test_kekse/Test_Kekse.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using kekse;
+using NUnit.Framework;
 
 namespace test_kekse
 {
+    [TestFixture]
     public class Test_Kekse
     {
+        [Test, Category("Akzeptanztest")]
+        public void Zahlen_zwischen_Untergrenze_Obergrenze()
+        {
+            var ergebnis = Kekse.Start_Zahlen_testen(3, 5);
+            Assert.AreEqual( new []{ 3,4,5}, ergebnis.ToArray());
+        }
+
+        [Test, Category("Gerüsttest")]
+        public void anzahl_zahlen()
+        {
+            var ergebnis = Kekse.Zahlen_testen(-1, 45);
+            Assert.AreEqual(47, ergebnis.Count());
+        }
     }
 }
