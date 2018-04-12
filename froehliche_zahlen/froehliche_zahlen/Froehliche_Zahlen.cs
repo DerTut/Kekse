2f1f5900a025d16133017b7c66438e6427194dd5
DerTut <fridtjofwilms@hotmail.de>
2018-04-12 13:26:44 +0200

    Fröhliche Zahlen
    + project "Fröhliche Zahlen"
    + function for Input
    + function for counting the numerics
    + list for numerics
    + numerics into list

---
+++ froehliche_zahlen/froehliche_zahlen/Froehliche_Zahlen.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace froehliche_zahlen
+{
+    public class Froehliche_Zahlen
+    {
+        public static void Eingabe_der_Zahl()
+        {
+            var zahl = Eingabe();
+            var getrennteZahl = Zahl_trennen(zahl);
+            var quadrate = Quadrate_berechnen();
+            var summe = Summe_berechen();
+        }
+
+        static int Eingabe()
+        {
+            Console.Write("Geben Sie eine Zahl ein: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static List<int> Zahl_trennen(int zahl)
+        {
+            var zifferanzahl = Zifferanzahl_ermitteln(zahl);
+            var liste = Liste_erstellen();
+            var gefüllte_liste = liste_füllen(liste, zahl, zifferanzahl);
+            return gefüllte_liste;
+        }
+        public static int Zifferanzahl_ermitteln(int zahl)
+        {
+            string zahlstring = Convert.ToString(zahl);
+            int ziffernanzahl = zahlstring.Length;
+            return ziffernanzahl;
+        }
+        public static List<int> Liste_erstellen()
+        {
+            List<int> Liste = new List<int>();
+            return Liste;
+        }
+        public static List<int> liste_füllen(List<int> liste, int zahl, int ziffernanzahl)
+        {
+            string zahlstring = Convert.ToString(zahl);
+            for (int i = 1; i <= ziffernanzahl; i++)
+            {
+                liste.Add(Convert.ToInt32(zahlstring.Substring(i, 1)));
+            }
+            return liste;
+        }
+    }
+}
