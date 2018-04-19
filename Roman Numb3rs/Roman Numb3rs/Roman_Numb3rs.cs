77b30e04ee6f304a75217ce59a7c074c788fafe0
DerTut <fridtjofwilms@hotmail.de>
2018-04-19 09:34:41 +0200

    Roman Nub3rs - functional
    • calculations in clean code (not perfect, but good enough)

--- Roman Numb3rs/Roman Numb3rs/Roman_Numb3rs.cs
+++ Roman Numb3rs/Roman Numb3rs/Roman_Numb3rs.cs
@@ -20,7 +20,6 @@ namespace Roman_Numb3rs
             var roemische_zahl = Berechnung(arab_num, woerterbuch);
             return roemische_zahl;
         }
-
         internal static Dictionary<int,string> Woerterbuch()
         {
             var woerterbuch_leer = Woerterbuch_erstellen();
@@ -52,85 +51,123 @@ namespace Roman_Numb3rs
         internal static string Berechnung(int arab_zahl, Dictionary<int,string> woerterbuch)
         {
             string roem_zahl = "";
-
-            roem_zahl = Tausend(arab_zahl, roem_zahl, woerterbuch);
-            roem_zahl = Neunhundert(arab_zahl, roem_zahl, woerterbuch);
-            roem_zahl = Fuenfhundert(arab_zahl, roem_zahl, woerterbuch);
-            roem_zahl = Vierhundert(arab_zahl, roem_zahl, woerterbuch);
-
-           
-            if (arab_zahl > 500)
-            {
-                arab_zahl = arab_zahl - 500;
-                roem_zahl = roem_zahl + woerterbuch[500];
-            }
-            if (arab_zahl > 400)
-            {
-                arab_zahl = arab_zahl - 400;
-                roem_zahl = roem_zahl + woerterbuch[400];
-            }
-            while (arab_zahl > 99)
-            {
-                arab_zahl = arab_zahl - 100;
-                roem_zahl = roem_zahl + woerterbuch[100];
-            }
-            if (arab_zahl > 90)
-            {
-                arab_zahl = arab_zahl - 90;
-                roem_zahl = roem_zahl + woerterbuch[90];
-            }
-            if (arab_zahl > 50)
-            {
-                arab_zahl = arab_zahl - 50;
-                roem_zahl = roem_zahl + woerterbuch[50];
-            }
-            if (arab_zahl > 40)
-            {
-                arab_zahl = arab_zahl - 40;
-                roem_zahl = roem_zahl + woerterbuch[40];
-            }
-            while (arab_zahl > 9)
-            {
-                arab_zahl = arab_zahl - 10;
-                roem_zahl = roem_zahl + woerterbuch[10];
-            }
-            if (arab_zahl == 9)
-            {
-                arab_zahl = arab_zahl - 9;
-                roem_zahl = roem_zahl + woerterbuch[9];
-            }
-            if (arab_zahl > 5)
-            {
-                arab_zahl = arab_zahl - 5;
-                roem_zahl = roem_zahl + woerterbuch[5];
-            }
-            if (arab_zahl > 4)
-            {
-                arab_zahl = arab_zahl - 4;
-                roem_zahl = roem_zahl + woerterbuch[4];
-            }
-            while (arab_zahl >= 1)
-            {
-                arab_zahl = arab_zahl - 1;
-                roem_zahl = roem_zahl + woerterbuch[1];
-            }
+            roem_zahl = Tausend_abziehen(arab_zahl, roem_zahl, woerterbuch);
             return roem_zahl;
         }
-        internal static string Tausend(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        internal static string Tausend_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
         {
             while (arab_zahl > 999)
             {
                 arab_zahl = arab_zahl - 1000;
                 roem_zahl = roem_zahl + woerterbuch[1000];
             }
-            return roem_zahl;
+            return Neunhundert_abziehen(arab_zahl, roem_zahl, woerterbuch);
         }
-        internal static string Neunhundert(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        internal static string Neunhundert_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
         {
             if (arab_zahl > 900)
             {
                 arab_zahl = arab_zahl - 900;
                 roem_zahl = roem_zahl + woerterbuch[900];
+            }
+            return Fuenfhundert_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Fuenfhundert_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            if (arab_zahl > 500)
+            {
+                arab_zahl = arab_zahl - 500;
+                roem_zahl = roem_zahl + woerterbuch[500];
+            }
+            return Vierhundert_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Vierhundert_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            if (arab_zahl > 400)
+            {
+                arab_zahl = arab_zahl - 400;
+                roem_zahl = roem_zahl + woerterbuch[400];
+            }
+            return Hundert_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Hundert_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            while (arab_zahl > 99)
+            {
+                arab_zahl = arab_zahl - 100;
+                roem_zahl = roem_zahl + woerterbuch[100];
+            }
+            return Neunzig_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Neunzig_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            if (arab_zahl > 90)
+            {
+                arab_zahl = arab_zahl - 90;
+                roem_zahl = roem_zahl + woerterbuch[90];
+            }
+            return Fuenfzig_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Fuenfzig_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            if (arab_zahl > 50)
+            {
+                arab_zahl = arab_zahl - 50;
+                roem_zahl = roem_zahl + woerterbuch[50];
+            }
+            return Vierzig_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Vierzig_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            if (arab_zahl > 40)
+            {
+                arab_zahl = arab_zahl - 40;
+                roem_zahl = roem_zahl + woerterbuch[40];
+            }
+            return Zehn_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Zehn_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            while (arab_zahl > 9)
+            {
+                arab_zahl = arab_zahl - 10;
+                roem_zahl = roem_zahl + woerterbuch[10];
+            }
+            return Neun_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Neun_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            if (arab_zahl == 9)
+            {
+                arab_zahl = arab_zahl - 9;
+                roem_zahl = roem_zahl + woerterbuch[9];
+            }
+            return Fuenf_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Fuenf_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            if (arab_zahl > 5)
+            {
+                arab_zahl = arab_zahl - 5;
+                roem_zahl = roem_zahl + woerterbuch[5];
+            }
+            return Vier_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Vier_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            if (arab_zahl > 4)
+            {
+                arab_zahl = arab_zahl - 4;
+                roem_zahl = roem_zahl + woerterbuch[4];
+            }
+            return Eins_abziehen(arab_zahl, roem_zahl, woerterbuch);
+        }
+        internal static string Eins_abziehen(int arab_zahl, string roem_zahl, Dictionary<int, string> woerterbuch)
+        {
+            while (arab_zahl >= 1)
+            {
+                arab_zahl = arab_zahl - 1;
+                roem_zahl = roem_zahl + woerterbuch[1];
             }
             return roem_zahl;
         }
